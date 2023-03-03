e456d45a57bb977b361e4ea14457b9f1701a3b78
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-03-03 12:52:45 +0100

    add cookies for login

--- src/components/TokenProvider.js
+++ src/components/TokenProvider.js
@@ -1,11 +1,27 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react";
 
-export const TokenContext = createContext(null)
+import { getCookie } from "react-use-cookie";
+
+export const TokenContext = createContext(null);
 
 export default function TokenProvider({ children }) {
-	const [token, setToken] = useState(null)
+  const [token, setToken] = useState(null);
 
-	return <TokenContext.Provider value={{token, setToken}}>
-		{children}
-	</TokenContext.Provider>
+  useEffect(
+    function () {
+      if (token === null) {
+        const refreshToken = getCookie("trainer-cookie");
+        if (refreshToken) {
+          setToken(JSON.parse(refreshToken));
+        }
+      }
+    },
+    [token]
+  );
+
+  return (
+    <TokenContext.Provider value={{ token, setToken }}>
+      {children}
+    </TokenContext.Provider>
+  );
 }
