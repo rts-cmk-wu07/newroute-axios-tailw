54140bfc1cf84345d98dfe42114e40d2c3b86369
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-03-06 00:42:49 +0100

    test Get

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const [classes, setClasses] = useState;
+  //   const [isLoading, setIsLoading] = useState;
+  //   const [error, setError] = useState;
+
+  useEffect(function () {
+    (async function () {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/classes"
+        );
+        console.log(response);
+        if (response.status === 200) {
+          setClasses(response.data);
+        }
+      } catch (error) {
+        //setError(error);
+        //console.log(error);
+      } finally {
+        //setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  //console.log("klasser", classes);
+
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
 }
