6d2cdd379ea64452286e661efc62b90837e91029
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-03-07 20:56:45 +0100

    add / before profile path in route

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         element: <Login />,
       },
       {
-        path: "profile",
+        path: "/profile",
         element: (
           <ProtectedRoute>
             <Profile />
